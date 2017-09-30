670c5daa78e5219999bd075d9182497f331dbae4
Luke Bainbridge <luke.bainbridge@gmail.com>
2017-09-30 09:57:38 -0400

    Refactoring user admin into workable solution.

--- Controller/UserAdminController.php
+++ Controller/UserAdminController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class UserController extends Controller
+class UserAdminController extends Controller
 {
     public function indexAction(Request $request)
     {
