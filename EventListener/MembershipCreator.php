6eb77fa42bd32076d7eb09215e1f81bbe8133da3
Luke Bainbridge <luke.bainbridge@gmail.com>
2017-09-30 09:38:19 -0400

    Adding flash message on membership auto-creation.

--- EventListener/MembershipCreator.php
+++ EventListener/MembershipCreator.php
@@ -7,32 +7,56 @@ use Bkstg\FOSUserBundle\Entity\ProductionMembership;
 use Bkstg\FOSUserBundle\Entity\User;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use MidnightLuke\GroupSecurityBundle\Model\GroupMembershipInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class MembershipCreator
 {
     private $token_storage;
-    public function __construct(TokenStorageInterface $token_storage) {
+    private $session;
+    private $translator;
+
+    public function __construct(
+        TokenStorageInterface $token_storage,
+        SessionInterface $session,
+        TranslatorInterface $translator
+    ) {
         $this->token_storage = $token_storage;
+        $this->session = $session;
+        $this->translator = $translator;
     }
 
+    /**
+     * When a user creates a production make them an admin in the production.
+     */
     public function prePersist(LifecycleEventArgs $args)
     {
         $object = $args->getObject();
         $user = $this->token_storage->getToken()->getUser();
 
-        // only act on some "Product" entity
+        // Only act on "Production" entities.
         if (!$object instanceof Production
           || !$user instanceof User) {
             return;
         }
 
         $om = $args->getObjectManager();
+
+        // Create and persist new membership.
         $membership = new ProductionMembership();
         $membership->setGroup($object);
         $membership->setMember($user);
         $membership->setStatus(GroupMembershipInterface::STATUS_ACTIVE);
         $membership->addRole('GROUP_ROLE_ADMIN');
         $om->persist($membership);
+
+        // Set message.
+        $this->session->getFlashBag()->add(
+            'success',
+            $this->translator->trans('You have been made a member of "%production%".', [
+                '%production%' => $object->getName(),
+            ])
+        );
     }
 }
