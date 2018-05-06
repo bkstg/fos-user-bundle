1bc2f936bb2d366cd12724959ece3c649cdb662a
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-05-06 09:26:06 -0400

    Remove validator pass, it was a dupe.

--- BkstgFOSUserBundle.php
+++ BkstgFOSUserBundle.php
@@ -2,16 +2,8 @@
 
 namespace Bkstg\FOSUserBundle;
 
-use Bkstg\FOSUserBundle\DependencyInjection\Compiler\ValidatorPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BkstgFOSUserBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new ValidatorPass());
-    }
 }
