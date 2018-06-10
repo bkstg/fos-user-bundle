739b53536548acf067c56908b02264d12373bf25
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-10 12:45:40 -0400

    Production profiles include roles now.

--- Controller/ProductionProfileController.php
+++ Controller/ProductionProfileController.php
@@ -5,6 +5,7 @@ namespace Bkstg\FOSUserBundle\Controller;
 use Bkstg\CoreBundle\Controller\Controller;
 use Bkstg\CoreBundle\Entity\Production;
 use Bkstg\CoreBundle\Util\ProfileManagerInterface;
+use Bkstg\FOSUserBundle\Entity\ProductionMembership;
 use Bkstg\FOSUserBundle\Entity\User;
 use Bkstg\FOSUserBundle\Form\Type\ProfileType;
 use Knp\Component\Pager\PaginatorInterface;
@@ -52,8 +53,50 @@ class ProductionProfileController extends Controller
 
         // Render the response.
         $users = $paginator->paginate($query, $request->query->getInt('page', 1));
-        return new Response($this->templating->render('@BkstgFOSUser/Profile/index.html.twig', [
+        return new Response($this->templating->render('@BkstgFOSUser/ProductionProfile/index.html.twig', [
+            'production' => $production,
             'users' => $users,
         ]));
+    }
+
+
+    /**
+     * Render a profile.
+     *
+     * @param int $id
+     *   The profile id to render.
+     *
+     * @return Response
+     *   The rendered profile.
+     */
+    public function readAction($production_slug, $profile_slug)
+    {
+        // Lookup the profile.
+        $user_repo = $this->em->getRepository(User::class);
+        if (null === $user = $user_repo->findOneBy(['slug' => $profile_slug])) {
+            throw new NotFoundHttpException();
+        }
+
+        // Lookup the production by production_slug.
+        $production_repo = $this->em->getRepository(Production::class);
+        if (null === $production = $production_repo->findOneBy(['slug' => $production_slug])) {
+            throw new NotFoundHttpException();
+        }
+
+        // Get the membership for this user in this group.
+        $membership_repo = $this->em->getRepository(ProductionMembership::class);
+        if (null === $membership = $membership_repo->findOneBy(['member' => $user, 'group' => $production])) {
+            throw new NotFoundHttpException();
+        }
+
+        // Render the response.
+        return new Response($this->templating->render(
+            '@BkstgFOSUser/ProductionProfile/show.html.twig',
+            [
+                'user' => $user,
+                'production' => $production,
+                'membership' => $membership,
+            ]
+        ));
     }
 }
