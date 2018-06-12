36c8569fc7672e7da57f104376f9b81a1fe4e91b
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-12 12:46:56 -0400

    Better membership provider.

--- User/MembershipProvider.php
+++ User/MembershipProvider.php
@@ -4,6 +4,8 @@ namespace Bkstg\FOSUserBundle\User;
 
 use Bkstg\CoreBundle\Entity\Production;
 use Bkstg\CoreBundle\User\MembershipProviderInterface;
+use Bkstg\CoreBundle\User\ProductionMembershipInterface;
+use Bkstg\CoreBundle\User\UserInterface;
 use Bkstg\FOSUserBundle\Entity\ProductionMembership;
 use Doctrine\ORM\EntityManagerInterface;
 use MidnightLuke\GroupSecurityBundle\Model\GroupInterface;
@@ -11,22 +13,51 @@ use MidnightLuke\GroupSecurityBundle\Model\GroupInterface;
 class MembershipProvider implements MembershipProviderInterface
 {
     private $em;
+    private $repo;
 
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
+        $this->repo = $this->em->getRepository(ProductionMembership::class);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function loadMembershipsByGroup(GroupInterface $group)
+    public function loadMembership(Production $production, UserInterface $user): ?ProductionMembershipInterface
     {
-        if (!$group instanceof Production) {
-            return [];
-        }
+        return $this->repo->findOneBy(['member' => $user, 'group' => $production]);
+    }
 
-        $membership_repo = $this->em->getRepository(ProductionMembership::class);
-        return $membership_repo->findMembershipsByGroup($group);
+    /**
+     * {@inheritdoc}
+     */
+    public function loadActiveMembershipsByProduction(Production $production)
+    {
+        return $this->repo->findAllActive($production);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadActiveMembershipsByUser(UserInterface $user)
+    {
+        return $this->repo->findActiveMemberships($user);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadAllMembershipsByProduction(Production $production)
+    {
+        return $this->repo->findBy(['group' => $production]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadAllMembershipsByUser(UserInterface $user)
+    {
+        return $this->repo->findBy(['member' => $user]);
     }
 }
