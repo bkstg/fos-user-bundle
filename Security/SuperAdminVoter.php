9b6b241a78d5b9a32693e31efad97d971392a98f
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-26 12:49:55 -0400

    Remove unneeded voter and comment super admin voter.

--- Security/SuperAdminVoter.php
+++ Security/SuperAdminVoter.php
@@ -10,20 +10,30 @@ class SuperAdminVoter extends Voter
 {
     /**
      * {@inheritdoc}
+     *
+     * @param  mixed $attribute The attribute to vote on.
+     * @param  mixed $subject   The subject to vote on.
+     * @return boolean
      */
-    protected function supports($attribute, $subject)
+    protected function supports($attribute, $subject): bool
     {
         return true;
     }
 
     /**
-     * {@inheritdoc}
+     * This voter allows users with the super admin role access to all ops.
+     *
+     * @param mixed          $attribute The attribute to vote on.
+     * @param mixed          $subject   The subject to vote on.
+     * @param TokenInterface $token     The token to vote using.
+     * @return boolean
      */
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         // Get the current user from the token.
         $user = $token->getUser();
 
+        // If this user has the super admin role always allow all operations.
         if ($user instanceof User && $user->hasRole('ROLE_SUPER_ADMIN')) {
             return true;
         }
