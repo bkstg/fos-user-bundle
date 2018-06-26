b2e0e7f475adf19d5aa1eb6ef52d2f16324652d9
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-26 12:44:44 -0400

    UserType updates.

--- Form/UserType.php
+++ Form/UserType.php
@@ -2,6 +2,7 @@
 
 namespace Bkstg\FOSUserBundle\Form;
 
+use Bkstg\FOSUserBundle\BkstgFOSUserBundle;
 use Bkstg\FOSUserBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -13,28 +14,48 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @param  FormBuilderInterface $builder The form builder.
+     * @param  array                $options The form options.
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('email', EmailType::class)
+            ->add('username', TextType::class, [
+                'label' => 'user.form.username',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'user.form.email',
+            ])
             ->add('roles', ChoiceType::class, [
+                'label' => 'user.form.roles',
                 'multiple' => true,
                 'required' => false,
                 'choices' => [
-                    'Editor' => 'ROLE_EDITOR',
-                    'Admin' => 'ROLE_ADMIN',
+                    'user.form.roles_choices.editor' => 'ROLE_EDITOR',
+                    'user.form.roles_choices.admin' => 'ROLE_ADMIN',
                 ],
             ])
             ->add('enabled', CheckboxType::class, [
+                'label' => 'user.form.enabled',
                 'required' => false,
             ])
         ;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @param OptionsResolver $resolver The options resolver.
+     * @return void
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            'translation_domain' => BkstgFOSUserBundle::TRANSLATION_DOMAIN,
             'data_class' => User::class,
         ]);
     }
